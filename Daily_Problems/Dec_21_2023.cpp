14098c72976e22018ef9a934a307292e40c4b36f
Rajar12345 <124551189+Rajar12345@users.noreply.github.com>
2023-12-21 11:39:13 +0530

    Create Dec_21_2023.cpp

---
+++ Daily_Problems/Dec_21_2023.cpp
@@ -0,0 +1,17 @@
+int minCandy(int N, vector<int> &ratings) {
+        // code here
+        vector<int>a(N,1);
+        int ans=0;
+        for(int i=1;i<N;i++){
+            if(ratings[i]>ratings[i-1]){
+                a[i]=a[i-1]+1;
+            }
+        }
+        for(int i=N-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]&&a[i]<=a[i+1]){
+                a[i]=a[i+1]+1;
+            }
+        }
+        for(int i=0;i<N;i++) ans+=a[i];
+        return ans;
+    }

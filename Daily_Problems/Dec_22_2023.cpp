f3541aa6938541da6cbb4723701be9ee8aa8572e
Rajar12345 <124551189+Rajar12345@users.noreply.github.com>
2023-12-22 09:22:01 +0530

    Create Dec_22_2023.cpp

---
+++ Daily_Problems/Dec_22_2023.cpp
@@ -0,0 +1,19 @@
+public:
+    vector<int> maxMeetings(int N,vector<int> &S,vector<int> &F){
+        vector<pair<pair<int,int>,int>>v;
+        for(int i=0;i<N;i++) {
+            v.push_back({{F[i],S[i]},i+1});
+        }
+        sort(v.begin(),v.end());
+        vector<int>ans;
+        int end = v[0].first.first;
+        ans.push_back(v[0].second);
+        for(int i=1;i<N;i++) {
+            if(v[i].first.second>end) {
+                end = v[i].first.first;
+                ans.push_back(v[i].second);
+            }
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }

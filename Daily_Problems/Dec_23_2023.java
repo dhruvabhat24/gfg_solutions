7b922c293406361c7c286ee3bbffb036091814f3
Rajar12345 <124551189+Rajar12345@users.noreply.github.com>
2023-12-23 11:17:15 +0530

    Create Dec_23_2023.java

---
+++ Daily_Problems/Dec_23_2023.java
@@ -0,0 +1,17 @@
+class Solution 
+{
+    //Function to find all elements in array that appear more than n/k times.
+    public int countOccurence(int[] arr, int n, int k) 
+    {
+        // your code here,return the answer
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int num: arr) {
+            map.put(num, map.getOrDefault(num, 0)+1);
+        }
+        int cnt = 0;
+        for(int count: map.values()) {
+            if(count > n/k) cnt++;
+        }
+        return cnt;
+    }
+}

d6c79522d87cb88f452b7753d9135b906f2db03a
Rajar12345 <124551189+Rajar12345@users.noreply.github.com>
2024-01-10 13:13:42 +0530

    Create Jan_10_2024.java

---
+++ Daily_Problems/Jan_10_2024.java
@@ -0,0 +1,11 @@
+HashMap<Integer, Integer> mp = new HashMap<>();
+        int ans = 0, sum = 0;
+        mp.put(0, -1);
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+            int r = sum % k;
+            if (r < 0) r += k; 
+            if (!mp.containsKey(r)) mp.put(r, i);
+            else ans = Math.max(ans, i - mp.get(r));
+        }
+        return ans;

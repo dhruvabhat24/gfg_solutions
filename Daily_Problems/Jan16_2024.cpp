1d32addd7cf228ca58ec7af3178db97e8e2dca1b
Rajar12345 <124551189+Rajar12345@users.noreply.github.com>
2024-01-16 10:23:22 +0530

    Create Jan16_2024.cpp

---
+++ Daily_Problems/Jan16_2024.cpp
@@ -0,0 +1,18 @@
+static int numberSequence(int m, int n)
+    {
+        // code here
+        if(n==0)
+        return 0;
+        
+        if(n==1)
+        return m;
+        
+        int sum=0;
+        
+        for(int i=1;i<=m;i++){
+            
+            sum+=numberSequence(i/2,n-1);
+        }
+        
+        return sum;
+    }
